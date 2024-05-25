5300cd9bc2e6916b988906466e1bc8d0b5648b58
Andry Ariadi <andryariadi23@gmail.com>
2024-05-25 12:47:27 +0700

    add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
   return (
     <>
+      <div className="bg-rose-500 pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button>Something</Button>
+      </div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
