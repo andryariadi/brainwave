bd1b75a68bb2d6a8a9305a2d0e841d0737dc5fc6
Andry Ariadi <andryariadi23@gmail.com>
2024-05-25 16:25:53 +0700

    setup mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <div className="bg-rose-500 pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
       </div>
 
