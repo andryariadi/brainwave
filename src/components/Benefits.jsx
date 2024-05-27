1d6aecf3d958a5d709c386185f0335576e4814d1
Andry Ariadi <andryariadi23@gmail.com>
2024-05-27 20:15:41 +0700

    add reusable heading component

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,9 +1,30 @@
+import { benefits } from "../constants";
+import Heading from "./Heading";
 import Section from "./Section";
 
 const Benefits = () => {
   return (
     <Section id="#features" className="border-4 border-teal-500">
-      <div className="container relative z-2">Andry</div>
+      <div className="bg-amber-500 container relative z-2">
+        <Heading title="Chat Smarter, Not Harder with Brainwave" className="bg-sky-500 max-w-md lg:max-w-2xl" />
+
+        <div className="bg-violet-500 flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              key={item.id}
+              className="bg-rose-500 block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+            >
+              <div className="bg-amber-800 relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h2 mb-5">{item.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </Section>
   );
 };
